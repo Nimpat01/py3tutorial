1f9d58f9a1c4759b9327262bdf62034de3021101
nimpat01 <nim199255@gmail.com>
2020-06-17 19:45:46 +0530

    Added Few more contidion and Explantion in exercise

--- Python/py3function.py
+++ Python/py3function.py
@@ -1,20 +1,42 @@
 '''Q) What is Function
    A) A Function is a block of code which is reusable 
-   to perform same operation'''
+   to perform same operation
+   The keyword 'def' introduces a function defination'''
+'''The global statement
+   The global statement is a declaration which holds for the entire current code block. 
+   It means that the listed identifiers are to be interpreted as globals.
+   It would be impossible to assign to a global variable without global.
+   Free variables may refer to globals without being declared global.
+   The nonlocal statement
+   The nonlocal statement causes the listed identifiers to refer to previously bound 
+   variables in the nearest enclosing scope excluding globals.
+   This is important because the default behavior for binding is to search the local namespace first.'''
 #Lets define function which create fibonacci series
 #Here num is number of fibonacci element given as output of this function
-#0 1 1 2
+
 def fibonacci_num(num):
-	a=0
-	b=1
+	'''This function will print "num" fibonacci element of fibonacci series '''
 	fibonacci=[]
-	fibonacci.append(0)
-	fibonacci.append(1)
-	if(num>1):
+	if(num==1):
+		fibonacci.append(0)
+		print(fibonacci)
+	elif(num==2):
+		fibonacci.append(0)
+		fibonacci.append(1)
+		print(fibonacci)			
+	elif(num>2):
+		a=0
+		b=1
+		fibonacci.append(0)
+		fibonacci.append(1)
 		for i in range (2,num):
 			fibonacci.append(a+b)
 			a=b
 			b=fibonacci[i]
-	print(fibonacci)	
+		print(fibonacci)			
+	else:
+		print("Wrong parameter")		
 
+
+fibonacci_num(0)
 fibonacci_num(50)
